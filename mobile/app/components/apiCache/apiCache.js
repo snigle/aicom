326a41725e1bfcd7cc7ce88c38724af53e5e1e59
Ludovic Lamarche <snigle64@gmail.com>
2017-08-22 08:26:35 +0200

    add mutex in api cache

--- mobile/app/components/apiCache/apiCache.js
+++ mobile/app/components/apiCache/apiCache.js
@@ -4,64 +4,88 @@ import moment from "moment";
 
 export default class ApiCache {
   constructor(prefix, delay = 3600) {
+    let self = this;
     console.log("cache constructor", prefix, delay);
-    this.prefix = prefix;
-    this.delay = delay;
+    self.prefix = prefix;
+    self.delay = delay;
+    self.mutex = Promise.resolve();
   }
 
   _addPrefix(key) {
-    return `${this.key}$$${key}`;
+    return `${self.key}$$${key}`;
   }
 
   reset() {
     let self = this;
-    return this._getKeys().then((keys) => {
-      console.log("reset cache", keys);
-      return Promise.all(_.mapKeys(keys), key => {
-        return AsyncStorage.removeItem(self._addPrefix(key));
-      }).then(() => AsyncStorage.removeItem(self._addPrefix("cache_keys")));
-    }).catch((err) => console.log("fail to reset cache", err));
+    return self.mutex.then(() => {
+      let promise = self._getKeys().then((keys) => {
+        console.log("reset cache", keys);
+        return Promise.all(_.mapKeys(keys), key => {
+          return AsyncStorage.removeItem(self._addPrefix(key));
+        }).then(() => AsyncStorage.removeItem(self._addPrefix("cache_keys")));
+      }).catch((err) => console.log("fail to reset cache", err));
+      self.mutex = promise;
+      return promise;
+    });
   }
 
   set(key, data) {
     let self = this;
-    return this._getKeys().then((keys) => {
-      console.log("set cache", key, keys, keys[key]);
-      keys[key] = moment();
-      self._saveKeys(keys);
-      return AsyncStorage.setItem(this._addPrefix(key), data);
+    return self.mutex.then(() => {
+      let promise = self._getKeys().then((keys) => {
+        console.log("set cache", key, keys, keys[key]);
+        keys[key] = moment();
+        self._saveKeys(keys);
+        return AsyncStorage.setItem(self._addPrefix(key), data);
+      });
+      self.mutex = promise;
+      return promise;
     });
   }
 
   _getKeys() {
-    return AsyncStorage.getItem(this._addPrefix("cache_keys")).then((keys) => {
-      if (keys) {
-        return _.mapValues(JSON.parse(keys), (value) => moment(value));
-      }
-      return {};
-    }, () => { return {}; });
+    let self = this;
+    return self.mutex.then(() => {
+      let promise = AsyncStorage.getItem(self._addPrefix("cache_keys")).then((keys) => {
+        if (keys) {
+          return _.mapValues(JSON.parse(keys), (value) => moment(value));
+        }
+        return {};
+      }, () => { return {}; });
+      self.mutex = promise;
+      return promise;
+    });
   }
 
   _saveKeys(keys) {
     console.log("save keys", keys);
-    if (keys) {
-      return this._getKeys().then((cachedKeys) =>
-        AsyncStorage.setItem(this._addPrefix("cache_keys"), JSON.stringify({ ...cachedKeys, ...keys }))
-      );
-    }
-    return Promise.resolve(null);
+    let self = this;
+    return self.mutex.then(() => {
+      let promise = null;
+      if (keys) {
+        promise = self._getKeys().then((cachedKeys) =>
+        AsyncStorage.setItem(self._addPrefix("cache_keys"), JSON.stringify({ ...cachedKeys, ...keys })));
+      }
+      self.mutex = promise;
+      return promise;
+    });
   }
 
   get(key) {
-    return this._getKeys().then((keys) => {
-      console.log("read cache", key, keys[key], this.delay, keys[key] && keys[key].clone().add(this.delay, "s").isBefore(moment()));
-      if (!keys[key] || keys[key].clone().add(this.delay, "s").isBefore(moment())) {
-        return Promise.resolve(null);
-      }
-      return AsyncStorage.getItem(this._addPrefix(key)).then((resp) => {
-        console.log("read cache successful");
-        return resp;
+    let self = this;
+    return self.mutex.then(() => {
+      let promise =  self._getKeys().then((keys) => {
+        console.log("read cache", key, keys[key], self.delay, keys[key] && keys[key].clone().add(self.delay, "s").isBefore(moment()));
+        if (!keys[key] || keys[key].clone().add(self.delay, "s").isBefore(moment())) {
+          return Promise.resolve(null);
+        }
+        return AsyncStorage.getItem(self._addPrefix(key)).then((resp) => {
+          console.log("read cache successful");
+          return resp;
+        });
       });
+      self.mutex = promise;
+      return promise;
     });
   }
 
